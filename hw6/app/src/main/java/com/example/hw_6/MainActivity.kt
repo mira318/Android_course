ac2d62b3be4323d88634373975de6023fb33750b
Chuviliaeva Irina <chuviliaeva.ip@phystech.edu>
2021-12-25 21:20:26 +0300

    hw7 -- view switching

--- hw6/app/src/main/java/com/example/hw_6/MainActivity.kt
+++ hw6/app/src/main/java/com/example/hw_6/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.hw_6
 
-import android.content.Intent.getIntent
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.leanback.widget.ImageCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +19,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val cardView = findViewById<androidx.cardview.widget.CardView>(R.id.card_view)
+        cardView.setCardBackgroundColor(ContextCompat.getColor(applicationContext,
+            R.color.card_view_background_color))
+        
         val dateText = findViewById<TextView>(R.id.data_string)
         val cardText = findViewById<TextView>(R.id.card_text)
         val NotesList = NotesRepository.getNotesList()
         dateText.text = NotesList[0].date
         cardText.text = NotesList[0].text
+
         cardView.setOnClickListener{
-            val intent = DetailedActivity.getIntent(this)
-            startActivity(intent)
+            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+                val intent = DetailedActivityLandscape.getIntent(this)
+                startActivity(intent)
+            } else {
+                // ориентация может быть ещё неопределённой
+                    // пусть в этих случаях тоже будет портретная
+                val intent = DetailedActivityPortrait.getIntent(this)
+                startActivity(intent)
+            }
+
+        }
+    }
+
+    companion object {
+        fun getIntent(context: Context?): Intent {
+            return Intent(context, MainActivity::class.java)
         }
     }
 }
